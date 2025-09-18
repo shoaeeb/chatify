4ad94c37571dde38fa0fe7c4964f960ff92a9c62
shoaeeb <osman.shoaeeb@gmail.com>
2025-09-18 09:53:03 +0530

    second commit

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,12 +1,21 @@
 //const express = require("express");
 import express from "express";
 import "dotenv/config";
+import path from "path";
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 
 const app = express();
 const PORT = process.env.PORT;
+const __dirname = path.resolve();
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`));
